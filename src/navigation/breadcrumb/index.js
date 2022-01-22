d1eef8f68e2dd0db107a9cd6bca973de78dfa59e
丘宇文 <80232265@qq.com>
2022-01-22 14:20:24 +0800

    feat(breadcrumb):面包屑组件

---
+++ src/navigation/breadcrumb/index.js
@@ -0,0 +1,44 @@
+import {$, createCustomElement, pxToVw, CreateHTMLElement, config} from '../../utils';
+import './item';
+
+/**
+ * 面包屑组件
+ */
+class BreadcrumbComponent extends CreateHTMLElement {
+    /**
+     * 渲染
+     * @returns {string}    返回html字符串
+     */
+    render() {
+        return `
+            <style>
+                :host {
+                    display: flex;
+                    flex-wrap: wrap;
+                }
+                
+                ::slotted(${config.prefix}-breadcrumb-item) {
+                    display: inline-flex;
+                    color: var(--color-gray-light);
+                }
+                
+                ::slotted(${config.prefix}-breadcrumb-item):last-of-type {
+                    color: var(--color-black);
+                }
+                
+                ::slotted(${config.prefix}-breadcrumb-item)::after {
+                    display: block;
+                    content: "${$(this).attr('separator') || '/'}";
+                    margin: ${pxToVw(0, 16)};
+                }
+                
+                ::slotted(${config.prefix}-breadcrumb-item:last-of-type):last-of-type::after {
+                    display: none;
+                }
+            </style>
+            <slot></slot>
+        `;
+    }
+}
+
+createCustomElement(BreadcrumbComponent);
---
+++ src/navigation/breadcrumb/item.js
@@ -0,0 +1,8 @@
+import {createCustomElement, CreateHTMLElement} from '../../utils';
+
+/**
+ * 面包屑子项
+ */
+class BreadcrumbItem extends CreateHTMLElement {}
+
+createCustomElement(BreadcrumbItem);
