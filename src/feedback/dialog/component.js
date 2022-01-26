5bddebb3c4163b534a14f4fedb7041f40c082dc8
丘宇文 <80232265@qq.com>
2022-01-26 10:38:12 +0800

    chore(/feedback/dialog):对话框组件

---
+++ src/feedback/dialog/component.js
@@ -0,0 +1,131 @@
+import {$, pxToVw, customElementsDefine, CreateHTMLElement, config} from '../../utils';
+import './../drawer';
+
+/**
+ * 对话框
+ * @docs    请查阅README.md文档
+ */
+@customElementsDefine
+class DialogComponent extends CreateHTMLElement {
+    /**
+     * 监听属性
+     * @returns {string[]}      需要被监听的属性名
+     */
+    static get observedAttributes() {
+        return ['visible'];
+    }
+
+    /**
+     * 获取是否可见
+     * @return {boolean}    ture || false
+     */
+    get visible() {
+        return $(this).attr('visible') === 'true';
+    }
+
+    /**
+     * 设置是否可见的
+     * @param bool          true || false
+     */
+    set visible(bool) {
+        $(this).attr('visible', bool === 'true' || bool === true);
+    }
+
+    /**
+     * 当自定义元素的指定属性被增加、移除或更改时被调用
+     * @param name          属性名
+     * @param oldValue      更改前的属性值
+     * @param newValue      新的属性值
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+            $(this.shadowRoot).find(`${config.prefix}-drawer`).attr('visible', newValue);
+        }
+    }
+
+    /**
+     * 当自定义元素第一次被连接到文档DOM时被调用
+     */
+    connectedCallback() {
+        this.onChange();
+    }
+
+    /**
+     * 当监听属性改变时
+     */
+    onChange() {
+        $(this.shadowRoot)
+            .find(`${config.prefix}-drawer`)
+            .on('change', ev => (this.visible = ev.detail.visible));
+    }
+
+    /**
+     * 自定义事件返回数据
+     */
+    CustomEventResultParams() {
+        return {
+            visible: this.visible,
+        };
+    }
+
+    /**
+     * 渲染
+     * @returns {string}    返回html字符串
+     */
+    render() {
+        let borderRadius = $(this).attr('border-radius') || 24;
+
+        return `
+            <${config.prefix}-drawer 
+                visible="${$(this).attr('visible') || false}"
+                mask-closable="${$(this).attr('mask-closable') || true}"
+                align="center"
+            >
+                <style>
+                    :host([visible]) {
+                        display: block !important;
+                    }
+                    
+                    .dialog-component {
+                        overflow: hidden;
+                        width: 520px;
+                        max-width: 85vw;
+                        padding-top: ${pxToVw(40)};
+                        background: white;
+                        border-radius: ${pxToVw(borderRadius || 24)};
+                    }
+                    
+                    ::slotted([slot=header]) {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        padding: ${pxToVw(0, 40)} !important;
+                        font-size: var(--font-size-md);
+                        font-weight: bold;
+                    }
+                    
+                    ::slotted([slot=body]) {
+                        overflow: auto;
+                        -webkit-overflow-scrolling: touch;
+                        max-height: 70vh;
+                        padding: ${pxToVw(10, 40, 0)} !important;
+                        word-break: break-all;
+                    }
+                    
+                    ::slotted([slot=footer]) {
+                        display: flex;
+                        align-items: center;
+                        margin-top: ${pxToVw(40)};
+                        border-top: var(--border-1px-width) solid var(--color-border);
+                    }
+                </style>
+                
+                <div class="dialog-component">
+                    <slot name="header"></slot>
+                    <slot name="body"></slot>
+                    <slot name="footer"></slot>
+                </div>
+            </${config.prefix}-drawer>
+        `;
+    }
+}
---
+++ src/feedback/dialog/index.js
@@ -0,0 +1,56 @@
+import {$, config} from '../../utils';
+import './component';
+
+/**
+ * 对话框
+ */
+export class Dialog {
+    constructor(options) {
+        this.id = `Dialog-${new Date().getTime()}`;
+
+        this.render(options);
+    }
+
+    /**
+     * 渲染
+     */
+    render(options) {
+        if (!options.title && !options.content) return;
+
+        let str = `
+            <${config.prefix}-dialog
+                id="${this.id}" 
+                visible="true"
+                mask-closable="${options.maskClosable || true}"
+                border-radius="${options.borderRadius || 24}"
+            >
+                ${options.title ? `<div slot="header">${options.title}</div>` : ``}
+                ${options.content ? `<div slot="body">${options.content}</div>` : ``}
+                
+                <div slot="footer">
+                    ${
+                        options.buttons?.length
+                            ? options.buttons.map((item, i) => `<button data-index="${i}">${item}</button>`)
+                            : `<button data-index="0">我知道了</button>`
+                    }
+                </div>
+            </${config.prefix}-drawer>
+        `;
+
+        $(document.body).append(str);
+
+        this.bind(options.onClick);
+    }
+
+    /**
+     * 事件绑定
+     * @param onClick   当操作按钮点击时回调
+     */
+    bind(onClick = () => {}) {
+        $(`#${this.id} [slot="footer"] button`).on('click', ev => {
+            let index = $(ev.target).attr('data-index');
+
+            onClick(Number(index), () => $(`#${this.id}`).attr('visible', false));
+        });
+    }
+}
