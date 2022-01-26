843010549a23216d34accfac3621c6f11afbc5b7
丘宇文 <80232265@qq.com>
2022-01-26 17:19:46 +0800

    chore(/utils/index):方法优化、新增Touch方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -247,8 +247,8 @@ class $$ {
      * @param i             DOM数组指定下标
      * @returns {*[]|*}     返回DOM数组或单个DOM
      */
-    get(i) {
-        if (typeof i !== 'undefined') {
+    get(i = "") {
+        if (i !== "" && this.selector.length) {
             i %= this.selector.length;
 
             if (i < 0) {
@@ -266,8 +266,8 @@ class $$ {
      * @param i             DOM数组指定下标
      * @returns {*[]|*}     返回DOM数组或单个DOM
      */
-    eq(i) {
-        if (typeof i !== 'undefined') {
+    eq(i = "") {
+        if (i !== "" && this.selector.length) {
             i %= this.selector.length;
 
             if (i < 0) {
@@ -297,26 +297,26 @@ class $$ {
      */
     /* eslint-disable */
     parents(selector, ...restOfSelector) {
+        let args = Array.from(arguments);
         let result = [];
 
-        arguments.forEach(arg => {
-            this.each(node => {
-                for (let i = 0; i < node.path.length; i++) {
-                    let item = node.path[i];
-                    if (item.nodeType >= 9) {
-                        break;
-                    }
+        this.each(node => {
+            while (node) {
+                if (node.nodeType >= 9 || node.parentNode.tagName.toLocaleLowerCase() === 'body') break;
 
+                for (let i = 0; i < args.length; i++) {
                     if (
-                        item.classList?.contains(arg) ||
-                        `#${item.id}` === arg ||
-                        item.tagName.toLocaleLowerCase() === arg
+                        node.classList?.contains(args[i].replace(/^./, '')) ||
+                        `#${node.id}` === args[i] ||
+                        node.tagName.toLocaleLowerCase() === args[i]
                     ) {
-                        result.push(item);
+                        result.push(node);
                         break;
                     }
                 }
-            });
+
+                node = node.parentNode;
+            }
         });
 
         this.selector = result;
@@ -663,6 +663,110 @@ class $$ {
  */
 export function $(options) {
     return new $$(options);
+}
+
+/**
+ * 触摸事件封装
+ */
+export class Touch {
+    constructor(options) {
+        this.data = {
+            touchstart: {
+                pageX: 0,
+                pageY: 0,
+            },
+            touchmove: {
+                pageX: 0,
+                pageY: 0,
+            },
+            touchend: {
+                pageX: 0,
+                pageY: 0,
+            },
+            moveDistance: {
+                pageX: 0,
+                pageY: 0,
+            },
+            rotate: 0,
+            scale: 1,
+            lock: false,
+        };
+
+        this.bindEvents(options);
+    }
+
+    /**
+     * 绑定事件
+     */
+    bindEvents(options) {
+        $(options.el)
+            .on('touchstart', ev => {
+                if (this.data.lock) return;
+
+                let touches = ev.touches ? ev.touches[0] : ev;
+
+                this.data.touchstart = {
+                    pageX: touches.pageX - this.data.touchend.pageX,
+                    pageY: touches.pageY - this.data.touchend.pageY,
+                };
+
+                this.data.moveDistance = {
+                    pageX: 0,
+                    pageY: 0,
+                };
+
+                let startResultValue = options?.onTouchStart?.call(ev.currentTarget, ev, this.data);
+
+                startResultValue
+                    ? (this.data.touchend = {
+                          ...startResultValue,
+                      })
+                    : '';
+            })
+            .on('touchmove', ev => {
+                if (this.data.lock) return;
+
+                let touches = ev.touches ? ev.touches[0] : ev;
+
+                this.data.touchmove = {
+                    pageX: touches.pageX - this.data.touchstart.pageX,
+                    pageY: touches.pageY - this.data.touchstart.pageY,
+                };
+
+                this.data.moveDistance = {
+                    pageX: this.data.touchmove.pageX - this.data.touchend.pageX,
+                    pageY: this.data.touchmove.pageY - this.data.touchend.pageY,
+                };
+
+                options?.onTouchMove?.call(ev.currentTarget, ev, this.data);
+            })
+            .on('touchend', ev => {
+                if (this.data.lock) return;
+
+                this.data.touchend = {
+                    ...this.data.touchmove,
+                };
+
+                let endResultValue = options?.onTouchEnd?.call(ev.currentTarget, ev, this.data);
+                endResultValue
+                    ? (this.data = {
+                          ...this.data,
+                          ...endResultValue,
+                      })
+                    : '';
+            });
+    }
+
+    set(object) {
+        this.data = {
+            ...this.data,
+            ...object,
+        };
+    }
+
+    get() {
+        return this.data;
+    }
 }
 
 /**
