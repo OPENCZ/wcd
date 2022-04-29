19e876dc86a17405f3a6c51ed3c27bc8253eb0f7
丘宇文 <80232265@qq.com>
2022-04-29 17:20:16 +0800

    chore(*):删除使用prettier格式化代码插件

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    semi: true,
-    proseWrap: 'preserve',
-    trailingComma: 'all',
-    bracketSpacing: true,
-    useTabs: true,
-    arrowParens: 'avoid',
-};
